6a47ccb30997190b2bec554a12d6ce56c5eb65f2
tdhood <taylordannhood@gmail.com>
2022-08-19 09:13:45 -0700

    turned off redirect

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"].replace(
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 toolbar = DebugToolbarExtension(app)
 
